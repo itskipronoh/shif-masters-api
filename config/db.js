570521f357ea17cd7ab92aba7ae66f9db0aa9245
Gideon-kipronoh <kipronogideon68@gmail.com>
2024-07-25 18:17:47 +0300

    database connection
    Signed-off-by: Gideon-kipronoh <kipronogideon68@gmail.com>

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
 };
 
-export default connectDB;
+module.exports = connectDB;
